7e81c4a4cc1c1034602d727e2d4d6955507da0ac
Rafael Braga <rafael.m.braga@hotmail.com>
2020-03-08 01:27:03 +0100

    Adding the whole project to my github

---
+++ lib/screens/results_page.dart
@@ -0,0 +1,71 @@
+import 'package:bmi_calculator/components/bottom_button.dart';
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/components/reusable_card.dart';
+
+class ResultsPage extends StatelessWidget {
+  ResultsPage(
+      {@required this.bmiResults,
+      @required this.interpretation,
+      @required this.resultText});
+
+  final String bmiResults;
+  final String resultText;
+  final String interpretation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your result',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              cardColor: kActiveCardColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    resultText.toUpperCase(),
+                    style: kResultsTextStyle,
+                  ),
+                  Text(
+                    bmiResults.toUpperCase(),
+                    style: kBMITextStyle,
+                  ),
+                  Text(
+                    interpretation.toUpperCase(),
+                    textAlign: TextAlign.center,
+                    style: kBodyTextStyle,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: 'RE-CALCULATE',
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
